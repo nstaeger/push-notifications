a79fb7da38260b5716071be2c53b545dd3809c3d
nstaeger <mail@nstaeger.de>
2016-09-22 16:57:35 +0200

    feat(LongPollingServer): increase size of thread pool to 10.000

--- server/de.nstaeger.pushnotifications.server.http-long-polling/src/main/java/de/nstaeger/pushnotifications/server/httplongpolling/LongPollingServer.java
+++ server/de.nstaeger.pushnotifications.server.http-long-polling/src/main/java/de/nstaeger/pushnotifications/server/httplongpolling/LongPollingServer.java
@@ -3,8 +3,10 @@ package de.nstaeger.pushnotifications.server.httplongpolling;
 import javax.servlet.http.HttpServlet;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 import de.nstaeger.pushnotifications.server.httplongpolling.notification.FakeNotificationService;
 
@@ -15,7 +17,11 @@ public class LongPollingServer extends Server
 {
     public LongPollingServer(final int port, final FakeNotificationService notificationService)
     {
-        super(port);
+        super(new QueuedThreadPool(10000));
+
+        final ServerConnector connector = new ServerConnector(this);
+        connector.setPort(port);
+        addConnector(connector);
 
         final HttpServlet servlet = new EventsServlet(notificationService);
 
